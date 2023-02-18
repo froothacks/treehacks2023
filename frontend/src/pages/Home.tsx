c49309f5edfd004c18b66d0a570377c0bd952810
Steven Xu <steven@level.com>
2023-02-18 03:39:34 -0800

    Cleanup

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { useMutation } from "src/convex/_generated/react";
 
 export const Home = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const sendMessage = useMutation("sendMessage");
+  const sendHello = () => sendMessage("Hello!", "me");
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button onClick={sendHello}>click me!</button>
+    </div>
+  );
 };
