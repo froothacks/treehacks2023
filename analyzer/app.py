7cf7ec62fd4f1c1c40776c3f9813d18dca7be994
coolcom200 <coolcom200@users.noreply.github.com>
2023-02-18 11:58:37 -0800

    Basic BoundingBoxService

--- analyzer/app.py
+++ analyzer/app.py
@@ -1,22 +1,36 @@
+import threading
+from multiprocessing import Process
+from pprint import pprint
+
+import cv2
+from convex import ConvexClient
 from flask import Flask
 from flask_restful import Resource, Api
-from convex import ConvexClient
+
 from config import url as convex_url
-from treehacks2023.analyzer.boundingBoxes import computeBoundingBox
+from parser import Parser
 
 app = Flask(__name__)
 api = Api(app)
 client = ConvexClient(convex_url)
+bbParser = Parser()
 client.debug = True
+
+
+def get_answers(image, bboxs):
+    response = bbParser.get_text_for_bounding_boxes(image, bboxs)
+    pprint(response)
 
 
 class BoundingBoxService(Resource):
     def get(self):
-        return computeBoundingBox()
+        blank_worksheet = cv2.imread('./test/W_NULL.jpg')
+        answer_key = cv2.imread('./test/W_ANS.jpg')
+        boxes = bbParser.get_answer_boxes(blank_worksheet, answer_key)
+        return boxes
 
 
-
-api.add_resource(BoundingBoxService, '/')
+api.add_resource(BoundingBoxService, '/answerkey')
 
 if __name__ == '__main__':
     app.run(debug=True)
