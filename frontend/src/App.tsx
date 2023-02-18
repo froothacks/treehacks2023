d371fe7175e2870573c543d3a528ba04ea0ca9e2
Steven Xu <steven@level.com>
2023-02-18 02:38:28 -0800

    Setup routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,17 @@
 import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Worksheet } from "./pages/Worksheet";
+import { Worksheets } from "./pages/Worksheets";
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/worksheets" element={<Worksheet />} />
+        <Route path="/worksheet" element={<Worksheets />} />
+      </Routes>
     </div>
   );
-}
-
-export default App;
-  
+};
