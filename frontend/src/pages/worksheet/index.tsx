0d16117a0caa7190928ebce54b72436464a781de
Steven Xu <steven@level.com>
2023-02-19 12:12:23 -0800

    Add more loading msgs

--- frontend/src/pages/worksheet/index.tsx
+++ frontend/src/pages/worksheet/index.tsx
@@ -14,6 +14,21 @@ import Lottie from "react-lottie-player";
 
 import animation from "src/static/animation.json";
 
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+const LOADING_TEXT_LIST = [
+  "Computing eigenvalues and calculating grades...",
+  "Optimizing hyperparameters and fine-tuning neural networks...",
+  "Training classifiers and analyzing feature importance...",
+  "Evaluating model accuracy and testing for overfitting...",
+  "Processing student responses with our cutting-edge AI technology...",
+  "Using machine learning algorithms to provide personalized feedback...",
+];
+
 export const Worksheet = () => {
   const params = useParams();
   const ws_id = params[QueryParams.WORKSHEET_ID] ?? "";
@@ -23,6 +38,15 @@ export const Worksheet = () => {
   );
   const [finishedLabelling, setFinishLabelling] = useState(false);
   const [startLabelling, setStartLabelling] = useState(false);
+  const [loadingTextIdx, setLoadingTextIdx] = useState<number>();
+
+  useInterval(() => {
+    let nextIdx = getRandomInt(0, LOADING_TEXT_LIST.length - 1);
+    while (nextIdx === loadingTextIdx) {
+      nextIdx = getRandomInt(0, LOADING_TEXT_LIST.length - 1);
+    }
+    setLoadingTextIdx(nextIdx);
+  }, 5000);
 
   console.log({ startLabelling, finishedLabelling });
   useInterval(() => {
@@ -34,14 +58,21 @@ export const Worksheet = () => {
 
   if (startLabelling && !finishedLabelling) {
     return (
-      <Box display="flex" flexDirection="column" justifyContent={"center"} alignItems="center">
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent={"center"}
+        alignItems="center"
+      >
         <Lottie
           loop
           animationData={animation}
           play
           style={{ width: 600, height: 600 }}
         />
-       <Heading as="h2" fontSize={20}>Computing eigenvalues and calculating grades...</Heading>
+        <Heading as="h2" fontSize={20}>
+          {loadingTextIdx !== undefined && LOADING_TEXT_LIST[loadingTextIdx]}
+        </Heading>
       </Box>
     );
   }
